8fec1f6d4b73e9866bf14b70d83cb7c58f84a4c1
aasem shoshari <aasemshoshari2013@hotmail.com>
2022-01-22 18:31:20 +0200

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <h2>Feedback IU</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,64 @@
+import { createContext, useState } from "react";
+import { v4 as uuidu4 } from "uuid";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: "this item is 1",
+      rating: 7,
+    },
+    {
+      id: 2,
+      text: "this item is 2",
+      rating: 5,
+    },
+    {
+      id: 3,
+      text: "this item is 3",
+      rating: 10,
+    },
+  ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidu4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({ item, edit: true });
+  };
+
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+        updateFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
